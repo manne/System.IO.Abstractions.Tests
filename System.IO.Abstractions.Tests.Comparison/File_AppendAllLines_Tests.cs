57a9e7e0aee047da801d260a2cf3cdb681d0a769
Manuel Pfemeter <manuel.pfemeter@gmx.de>
2016-06-19 13:37:51 +0200

    added first test

--- System.IO.Abstractions.Tests.Comparison/File_AppendAllLines_Tests.cs
+++ System.IO.Abstractions.Tests.Comparison/File_AppendAllLines_Tests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using System.IO.Abstractions.TestingHelpers;
+
+using FluentAssertions;
+using Xunit;
 
 namespace System.IO.Abstractions.Tests.Comparison
 {
@@ -7,6 +10,34 @@ namespace System.IO.Abstractions.Tests.Comparison
         [Fact]
         public void PathContainsInvalidCharacters()
         {
+            // Arrange
+            var mockFileSystem = new MockFileSystem();
+            var realFileSystem = new FileSystem();
+
+            // Act
+            Action<IFileSystem> action = fs => fs.File.AppendAllLines("|", new[] {"does not matter"});
+            Exception realException = null;
+            try
+            {
+                action(realFileSystem);
+            }
+            catch (Exception e)
+            {
+                realException = e;
+            }
+
+            Exception mockException = null;
+            try
+            {
+                action(mockFileSystem);
+            }
+            catch (Exception e)
+            {
+                mockException = e;
+            }
+
+            // Assert
+            mockException.Should().BeOfType(realException.GetType());
         }
     }
 }
